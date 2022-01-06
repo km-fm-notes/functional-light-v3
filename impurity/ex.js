9d79289ba15e275943c383b362b3d62864ee2250
kamarmack <kamarmack112@gmail.com>
2022-01-05 19:54:06 -0600

    completes impurity

--- impurity/ex.js
+++ impurity/ex.js
@@ -8,31 +8,37 @@ var students = [
 	{ id: 491, name: "Ally" }
 ];
 
-function sortStudentsByName() {
-	// Don't modify this function
-	students.sort(function byName(s1,s2){
-		if (s1.name < s2.name) return -1;
-		else if (s1.name > s2.name) return 1;
-		else return 0;
-	});
+// *************************************
+
+// wrapper
+function getStudentsByName(s) {
+	let students = [].concat(s);
+	sortStudentsByName();
 	return students;
+	
+	function sortStudentsByName() { // arrow syntax doesn't work
+		students.sort(function byName(s1,s2){
+			if (s1.name < s2.name) return -1;
+			else if (s1.name > s2.name) return 1;
+			else return 0;
+		});
+	}
 }
 
+// adapter
 function sortStudentsByID() {
-	// Don't modify this function
 	students.sort(function byID(s1,s2){
 		return s1.id - s2.id;
 	});
 	return students;
 }
-
-// *************************************
-
-// modify/move this function
-function getStudentsByName() { return students; }
-
-// modify/move this function
-function getStudentsByID() { return students; }
+function getStudentsByID(curStudents) {
+	var origStudents = students.slice(0);
+	students = curStudents;
+	var newStudents = sortStudentsByID();
+	students = origStudents;
+	return newStudents;
+}
 
 // *************************************
 
